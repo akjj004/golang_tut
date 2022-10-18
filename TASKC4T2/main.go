be0871e5e95cfa56283be7fdccfb230f13202d47
akjj004 <akjj004@gmail.com>
2022-10-18 14:12:36 +0200

    feat: done lab02

---
+++ TASKC4T2/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"text/template"
+)
+
+var items = [3]item{
+	{"OnePlus 10 Pro", 4500},
+	{"Asus X513EA", 2000},
+	{"Samsung UE55TU7022K", 1900},
+}
+
+func getItemID(url string) int {
+	fields := strings.Split(url, "/")
+	if len(fields) < 2 || fields[2] == "" {
+		return -1
+	} // - /item/
+	id, err := strconv.Atoi(fields[2])
+	if err != nil {
+		return -2
+	} // - /item/not_a_number
+	return id
+
+}
+
+// W pliku main.go utworzyć strukturę item
+// zawierającą następujące pola:
+// Name (string), Price (float)
+
+type item struct {
+	Name  string
+	Price float32
+}
+
+// Utworzyć serwer http obsługujący
+// stronę /item/ przez funkcję itemFunc.
+
+func itemFunc(w http.ResponseWriter, r *http.Request) {
+	// pobranie ID przedmiotu z adresu strony
+	id := getItemID(r.RequestURI)
+	fmt.Println(id)
+	if id < 0 {
+		// zwrócenie statycznej strony strona.html
+		http.ServeFile(w, r, "TASKC4T2/pages/strona.html")
+	}
+
+	if id >= 0 {
+		tmpl, _ := template.ParseFiles("TASKC4T2/pages/parse.html")
+		tmpl.Execute(w, &items[id])
+	}
+
+}
+
+func main() {
+	http.HandleFunc("/item/", itemFunc)
+	// http.HandleFunc("/parse/", itemFunc)
+	http.ListenAndServe("localhost:8080", nil)
+}
